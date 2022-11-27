2b13992e4a655be6fb9961243e53aa829f7c2227
Jean Breno <jeanbreno@ibm.com>
2022-11-27 10:36:11 -0300

    Mock de nomes.

--- Mock.cs
+++ Mock.cs
@@ -9,10 +9,10 @@ namespace Banco {
     public class Mock {
         public static void MockIncluir(List<Conta> contas) {
 
-            contas.Add(new Conta(1, "TESTE UM", 100));
-            contas.Add(new Conta(2, "TESTE DOIS", 200));
-            contas.Add(new Conta(3, "TESTE TRES", 300));
-            contas.Add(new Conta(4, "TESTE QUATRO", 400));
+            contas.Add(new Conta(1, "Joao Paulo", 100));
+            contas.Add(new Conta(2, "Demétrio Raposo", 200));
+            contas.Add(new Conta(3, "Joarez da Silva", 300));
+            contas.Add(new Conta(4, "Tariff Abudad", 400));
         }
     }
 }
